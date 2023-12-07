9dd74e8e3b2d85346fc935d4e9c12ce4cda8b7c8
Developer Shohan <dev.shohanur@gmail.com>
2023-12-07 23:50:32 +0600

    auth wrapper wip

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>,
 )
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import "./Auth.scss"
+
+import instaBackPic from "../../../public/instaBackPic.png"
+import instaLogo from "../../../public/instaLogo.png"
 
 const Auth = () => {
   return (
-    <div>Auth</div>
+    <div>
+      <div className="auth-container-wrapper">
+        <div className="row ">
+          <div className="col-md-6">
+            <img className='insta_left_img ' src={instaBackPic} alt="" />
+          </div>
+          <div className="col-md-6">
+            
+            <div className="card auth-login-box">
+              <div className="insta-img-top">
+              <img src={instaLogo} alt=""/>
+              </div>
+
+              <div className="card-body w-100 bg-success">
+<input className='w-100' type="text" name="" id="" />
+<input className='w-100' type="text" name="" id="" />
+                <p className="card-text">Body</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
